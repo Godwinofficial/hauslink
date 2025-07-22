d653f165bca3457780bd07d6055ce7735bfaa9f4
Godwin Banda <godwinbanda19@gmail.com>
2025-07-22 13:38:39 +0200

    fix5

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           {/* Auth Routes */}
           <Route element={<AuthLayout />}>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // must match your repo name!
+  base: '/hauslink/', // must match your repo name!
   server: {
     host: "::",
     port: 8080,
