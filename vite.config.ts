174e0b04b226cf04330dfc82b97361813de56422
Godwin Banda <godwinbanda19@gmail.com>
2025-07-22 13:28:20 +0200

    fix4

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/hauslink/', // must match your repo name!
+  base: '/', // must match your repo name!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    // mode === 'development' && componentTagger(), // removed
   ].filter(Boolean),
   resolve: {
     alias: {
