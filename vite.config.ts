9bc6b0fdd3f507160e0576bca17478d756f9ce9d
Godwin Banda <godwinbanda19@gmail.com>
2025-07-22 13:14:42 +0200

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/hauslink/', // must match your repo name!
   server: {
     host: "::",
@@ -12,8 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
